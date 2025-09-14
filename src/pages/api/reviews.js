5ed91d2434a8ef8c5e80945c0849ca16478f6e34
Pallishree <pallishreebehera01@gmail.com>
2025-09-14 20:55:17 +0530

    removed key from review page

--- src/pages/api/reviews.js
+++ src/pages/api/reviews.js
@@ -1,10 +1,10 @@
 // pages/api/reviews.js
 export default async function handler(req, res) {
   try {
-    const placeId = process.env.GOOGLE_PLACE_ID || 'ChIJRbnRwFeLPmQRv0b78Nz2cIc';
-    const apiKey = process.env.GOOGLE_API_KEY || 'AIzaSyCqtGE7I_VUwDFn0Xr7lH3McSellJZm7Nc';
-// https://places.googleapis.com/v1/places/ChIJRbnRwFeLPmQRv0b78Nz2cIc
-// https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJRbnRwFeLPmQRv0b78Nz2cIc&key=AIzaSyCqtGE7I_VUwDFn0Xr7lH3McSellJZm7Nc&fields=reviews
+    const placeId = process.env.GOOGLE_PLACE_ID;
+    const apiKey = process.env.GOOGLE_API_KEY;
+// https://places.googleapis.com/v1/places/
+// https://maps.googleapis.com/maps/api/place/details/json?place_id=&key=
 
     const response = await fetch(
       `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=name,rating,reviews&key=${apiKey}`
