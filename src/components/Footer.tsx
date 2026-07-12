f6c6ad63a9378f287c63b7afa838f7310d6fd5a1
sumitbadri2000 <sumitbadri2000@gmail.com>
2026-07-12 13:16:20 +0530

    change footer ui and add link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -47,9 +47,9 @@ const Footer = () => {
   return (
     <footer className="bg-[#111827] px-6 lg:px-16 2xl:px-32 3xl:px-48 py-12 2xl:py-16 3xl:py-20">
       <div className="max-w-7xl 3xl:max-w-[1600px] mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 2xl:gap-16 3xl:gap-20 mb-12">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-10 2xl:gap-16 3xl:gap-20 mb-12">
           {/* Company Info */}
-          <div className="space-y-1">
+          <div className="space-y-1 flex flex-col gap-2 lg:col-span-1">
             <div className="relative w-full max-w-70 2xl:max-w-80 h-10 2xl:h-12">
               <Image
                 src={
@@ -66,79 +66,8 @@ const Footer = () => {
               Quality from start to finish. From design through installation,
               IDR is the name you can depend on.
             </p>
-          </div>
 
-          {/* Product Links */}
-          <div>
-            <h3 className="text-lg 2xl:text-xl text-[#F7F6F2] mb-4 font-semibold">
-              Product
-            </h3>
-            <ul className="space-y-2">
-              <li>
-                <Link
-                  href="/portfolio"
-                  className="text-sm 2xl:text-base text-[#F7F6F2] hover:text-white transition"
-                >
-                  Features
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/contact"
-                  className="text-sm 2xl:text-base text-[#F7F6F2] hover:text-white transition"
-                >
-                  Request Demo
-                </Link>
-              </li>
-            </ul>
-          </div>
 
-          {/* Company Links */}
-          <div>
-            <h3 className="text-lg 2xl:text-xl text-[#F7F6F2] mb-4 font-semibold">
-              Company
-            </h3>
-            <ul className="space-y-2">
-              <li>
-                <Link
-                  href="/about"
-                  className="text-sm 2xl:text-base text-[#F7F6F2] hover:text-white transition"
-                >
-                  About
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/contact"
-                  className="text-sm 2xl:text-base text-[#F7F6F2] hover:text-white transition"
-                >
-                  Contact
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/faqs"
-                  className="text-sm 2xl:text-base text-[#F7F6F2] hover:text-white transition"
-                >
-                  FAQs
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/privacy-policy"
-                  className="text-sm 2xl:text-base text-[#F7F6F2] hover:text-white transition"
-                >
-                  Privacy Policy
-                </Link>
-              </li>
-            </ul>
-          </div>
-
-          {/* Connect */}
-          <div>
-            <h3 className="text-lg 2xl:text-xl text-[#F7F6F2] mb-6 font-semibold">
-              Connect
-            </h3>
             <div className="flex space-x-4 mb-6">
               {socialLinks.map((social, index) => (
                 <Link
@@ -157,6 +86,210 @@ const Footer = () => {
                   />
                 </Link>
               ))}
+            </div>
+          </div>
+
+          {/* our service Links */}
+          <div className="lg:col-span-2">
+            <h3 className="text-lg 2xl:text-xl text-[#F7F6F2] mb-4 font-semibold">
+              OUR SERVICES
+            </h3>
+            <div className="grid grid-cols-3 gap-4">
+              <ul className="space-y-2">
+                <li>
+                  <Link
+                    href="/services/access-control"
+                    className="text-sm 2xl:text-base text-[#F7F6F2] hover:text-white transition"
+                  >
+                    Access Control Systems
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/services/surveillance-camera"
+                    className="text-sm 2xl:text-base text-[#F7F6F2] hover:text-white transition"
+                  >
+                    Surveillance Camera Systems
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/camera-monitoring"
+                    className="text-sm 2xl:text-base text-[#F7F6F2] hover:text-white transition"
+                  >
+                    Camera Monitoring
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/services/intercom"
+                    className="text-sm 2xl:text-base text-[#F7F6F2] hover:text-white transition"
+                  >
+                    Multifamily Intercom Systems
+                  </Link>
+                </li>
+         
+              </ul>
+              <ul className="space-y-2 border-l border-white/20 pl-4">
+                       <li>
+                  <Link
+                    href="/services/security-alarm"
+                    className="text-sm 2xl:text-base text-[#F7F6F2] hover:text-white transition"
+                  >
+                    Security Alarm Systems
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/services/networks-it"
+                    className="text-sm 2xl:text-base text-[#F7F6F2] hover:text-white transition"
+                  >
+                    Networks & IT Infrastructure
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/services/structured-cabling"
+                    className="text-sm 2xl:text-base text-[#F7F6F2] hover:text-white transition"
+                  >
+                    Structured Cabling
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/services/data-center"
+                    className="text-sm 2xl:text-base text-[#F7F6F2] hover:text-white transition"
+                  >
+                    Data Center Infrastructure
+                  </Link>
+                </li>
+
+              </ul>
+
+              <ul className="space-y-2 border-l border-white/20 pl-4">
+                <li>
+                  <Link
+                    href="/services/e-rate-program"
+                    className="text-sm 2xl:text-base text-[#F7F6F2] hover:text-white transition"
+                  >
+                    E-Rate Program
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/services/audio-visual"
+                    className="text-sm 2xl:text-base text-[#F7F6F2] hover:text-white transition"
+                  >
+                    Audio / Visual Systems
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/services/conference-room"
+                    className="text-sm 2xl:text-base text-[#F7F6F2] hover:text-white transition"
+                  >
+                    Conference Room Solutions
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          {/* Company Links */}
+          <div>
+            <h3 className="text-lg 2xl:text-xl text-[#F7F6F2] mb-4 font-semibold">
+              Company
+            </h3>
+            <ul className="space-y-2">
+              <li>
+                <Link
+                  href="/about"
+                  className="text-sm 2xl:text-base text-[#F7F6F2] hover:text-white transition"
+                >
+                  About Us
+                </Link>
+              </li>
+              {/* <li>
+                <Link
+                  href="/contact"
+                  className="text-sm 2xl:text-base text-[#F7F6F2] hover:text-white transition"
+                >
+                  Contact
+                </Link>
+              </li> */}
+              <li>
+                <Link
+                  href="/portfolio"
+                  className="text-sm 2xl:text-base text-[#F7F6F2] hover:text-white transition"
+                >
+                  Features
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/contact"
+                  className="text-sm 2xl:text-base text-[#F7F6F2] hover:text-white transition"
+                >
+                  Demo Request
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/faqs"
+                  className="text-sm 2xl:text-base text-[#F7F6F2] hover:text-white transition"
+                >
+                  FAQs
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="https://portal.idrtechnologysolutions.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-sm 2xl:text-base text-[#F7F6F2] hover:text-white transition"
+                >
+                  IDR Portal
+                </Link>
+              </li>
+              {/* <li>
+                <Link
+                  href="/privacy-policy"
+                  className="text-sm 2xl:text-base text-[#F7F6F2] hover:text-white transition"
+                >
+                  Privacy Policy
+                </Link>
+              </li> */}
+            </ul>
+          </div>
+
+          {/* Connect */}
+          <div>
+            <h3 className="text-lg 2xl:text-xl text-[#F7F6F2] mb-6 font-semibold">
+              Connect
+            </h3>
+
+            <div className="flex items-start gap-3 mb-4">
+              <div className="text-red-500 ">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" style={{ transform: 'rotate(-90deg)' }}>
+                  <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
+                </svg>
+              </div>
+              <div className="text-sm 2xl:text-base text-[#F7F6F2] ">
+                <p>NY : (718) 594-7865</p>
+                <p>NU : (732) 217-3550</p>
+                <p>FL : (813) 685-4022</p>
+              </div>
+            </div>
+            <div className="flex items-start gap-3 mb-4">
+              <div className="text-red-500">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                  <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+                  <polyline points="22,6 12,13 2,6"></polyline>
+                </svg>
+              </div>
+              <div className="text-sm 2xl:text-base text-[#F7F6F2]">
+                <p>sales@idrtechnologysolutions.com</p>
+              </div>
             </div>
 
             {/* App Store Links */}
